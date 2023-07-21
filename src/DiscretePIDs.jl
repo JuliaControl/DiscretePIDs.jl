406266cce862bb484c776b84dd6c2975f49749da
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2023-07-21 14:58:12 +0200

    improve docstring

--- src/DiscretePIDs.jl
+++ src/DiscretePIDs.jl
@@ -34,6 +34,11 @@ u = K \\left( e + \\dfrac{1}{Ti} \\int e dt + T_d \\dfrac{de}{dt} \\right)
 U(s) = K \\left( bR(s) - Y(s) + \\dfrac{1}{sT_i} \\left( R(s) Y(s) \\right) - \\dfrac{sT_d}{1 + s T_d / N}Y(s)
 ```
 
+Call the controller like this
+```julia
+u = pid(r, y, uff) # uff is optional
+u = calculate_control!(pid, r, y, uff) # Equivalent to the above
+
 # Arguments:
 - `K`: Proportional gain
 - `Ti`: Integral time
@@ -48,7 +53,7 @@ U(s) = K \\left( bR(s) - Y(s) + \\dfrac{1}{sT_i} \\left( R(s) Y(s) \\right) - \\
 - `D`: Derivative part
 - `yold`: Last measurement signal
 
-See also [`calculate_control`](@ref), [`set_K!`](@ref), [`set_Ti!`](@ref), [`set_Td!`](@ref)
+See also [`calculate_control!`](@ref), [`set_K!`](@ref), [`set_Ti!`](@ref), [`set_Td!`](@ref)
 """
 function DiscretePID(;
     K::T  = 1f0,
